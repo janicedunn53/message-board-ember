acefa69faf1eb1aafaedf97eb215818c7c485153
Janice Dunn <janicedunn53@gmail.com>
2015-09-25 11:03:43 -0700

    Update a question's content, author, and notes.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestion: false,
   actions: {
+    update(question, params) {
+      this.sendAction('update', question, params);
+    },
+
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
