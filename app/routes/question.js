ce153892ea0fff0c473e2ee554879331b5fc337d
Janice Dunn <janicedunn53@gmail.com>
2015-10-02 15:14:45 -0700

    Add date to question post.

--- app/routes/question.js
+++ app/routes/question.js
@@ -24,17 +24,17 @@ export default Ember.Route.extend({
         question.save();
       });
       this.transitionTo('question');
-    }
+    },
 
-    // destroyQuestion(question) {
-    //   var answer_deletions = question.get('answers').map(function(answer) {
-    //     return answer.destroyRecord();
-    //   });
-    //   Ember.RSVP.all(answer_deletions)
-    //     .then(function() {
-    //       return question.destroyRecord();
-    //     })
-    //   this.transitionTo('index');
-    // }
+    destroyQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions)
+        .then(function() {
+          return question.destroyRecord();
+        })
+      this.transitionTo('index');
+    }
   }
 });
